d3c42da2ab50114ef0b6e5ac43a7f7c11e5f2fb8
Hưng Phan <phandinhhungvp2001@gmail.com>
2019-12-17 20:56:34 +0700

    Version Setting Info

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -78,10 +78,10 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
     public CharSequence getSummary() {
         StringBuilder sb = new StringBuilder();
         sb.append(BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY));
-        String pixelExperienceVersion = VersionUtils.getPixelExperienceVersion();
-        if (!pixelExperienceVersion.equals("")){
+        String doraemonVersion = VersionUtils.getdoraemonVersion();
+        if (!doraemonVersion.equals("")){
             sb.append("\n");
-            sb.append(pixelExperienceVersion);
+            sb.append(doraemonVersion);
         }
         return sb.toString();
     }
--- src/com/android/settings/deviceinfo/VersionUtils.java
+++ src/com/android/settings/deviceinfo/VersionUtils.java
@@ -4,7 +4,10 @@ package com.android.settings.deviceinfo;
 import android.os.SystemProperties;
 
 public class VersionUtils {
-    public static String getDoraemonOSVersion(){
-        return SystemProperties.get("ro.doraemon.version.display","");
+    public static String getdoraemonVersion(){
+        String buildDate = SystemProperties.get("ro.doraemon.build_date","");
+        String buildCherish = SystemProperties.get("ro.doraemon.version","");
+        String buildType = SystemProperties.get("ro.doraemon.build_type","unofficial")/*.toUpperCase()*/;
+        return buildDate.equals("") ? "" : "DoraemonOS-" + buildDate + "-" + buildType;
     }
 }
