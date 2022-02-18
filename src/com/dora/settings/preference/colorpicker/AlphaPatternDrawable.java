ConquerOS/packages_apps_Settings

colorpicker: preferences -> preference
Signed-off-by: Pulkit077 <pulkitagarwal2k1@gmail.com>

src/com/conquer/settings/preference/colorpicker/AlphaPatternDrawable.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.conquer.settings.preferences.colorpicker;
+package com.conquer.settings.preference.colorpicker;
 
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;

src/com/conquer/settings/preference/colorpicker/ColorPickerDialog.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.conquer.settings.preferences.colorpicker;
+package com.conquer.settings.preference.colorpicker;
 
 import android.app.AlertDialog;
 import android.content.Context;

src/com/conquer/settings/preference/colorpicker/ColorPickerPanelView.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.conquer.settings.preferences.colorpicker;
+package com.conquer.settings.preference.colorpicker;
 
 import android.content.Context;
 import android.graphics.Canvas;

src/com/conquer/settings/preference/colorpicker/ColorPickerPreference.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package com.conquer.settings.preferences.colorpicker;
+package com.conquer.settings.preference.colorpicker;
 
 import android.content.Context;
 import android.content.res.TypedArray;

src/com/conquer/settings/preference/colorpicker/SecureSettingColorPickerPreference.java
@@ -13,14 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.conquer.settings.preferences.colorpicker;
+package com.conquer.settings.preference.colorpicker;
 
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.provider.Settings;
 
-import com.conquer.settings.preferences.SecureSettingsStore;
+import com.conquer.settings.preference.SecureSettingsStore;
 
 public class SecureSettingColorPickerPreference extends ColorPickerPreference {
 

src/com/conquer/settings/preference/colorpicker/SystemSettingColorPickerPreference.java
@@ -13,14 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.conquer.settings.preferences.colorpicker;
+package com.conquer.settings.preference.colorpicker;
 
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.provider.Settings;
 
-import com.conquer.settings.preferences.SystemSettingsStore;
+import com.conquer.settings.preference.SystemSettingsStore;
 
 public class SystemSettingColorPickerPreference extends ColorPickerPreference {
 

DoraemonOS/android_packages_apps_Settings

Signed-off-by: dopaemon <polarisdp@gmail.com>

src/com/dora/settings/preference/colorpicker/AlphaPatternDrawable.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.dora.settings.preferences.colorpicker;
+package com.dora.settings.preference.colorpicker;
 
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;

src/com/dora/settings/preference/colorpicker/ColorPickerDialog.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.dora.settings.preferences.colorpicker;
+package com.dora.settings.preference.colorpicker;
 
 import android.app.AlertDialog;
 import android.content.Context;

src/com/dora/settings/preference/colorpicker/ColorPickerPanelView.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.dora.settings.preferences.colorpicker;
+package com.dora.settings.preference.colorpicker;
 
 import android.content.Context;
 import android.graphics.Canvas;

src/com/dora/settings/preference/colorpicker/ColorPickerPreference.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package com.dora.settings.preferences.colorpicker;
+package com.dora.settings.preference.colorpicker;
 
 import android.content.Context;
 import android.content.res.TypedArray;

src/com/dora/settings/preference/colorpicker/SecureSettingColorPickerPreference.java
@@ -13,14 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.dora.settings.preferences.colorpicker;
+package com.dora.settings.preference.colorpicker;
 
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.provider.Settings;
 
-import com.dora.settings.preferences.SecureSettingsStore;
+import com.dora.settings.preference.SecureSettingsStore;
 
 public class SecureSettingColorPickerPreference extends ColorPickerPreference {
 

src/com/dora/settings/preference/colorpicker/SystemSettingColorPickerPreference.java
@@ -13,14 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.dora.settings.preferences.colorpicker;
+package com.dora.settings.preference.colorpicker;
 
 import android.content.Context;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.provider.Settings;
 
-import com.dora.settings.preferences.SystemSettingsStore;
+import com.dora.settings.preference.SystemSettingsStore;
 
 public class SystemSettingColorPickerPreference extends ColorPickerPreference {
 